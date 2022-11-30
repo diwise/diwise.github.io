fe32fab7623ef2c576039439f4cd299cef9f2981
istyf <istyf@users.noreply.github.com>
2022-11-30 14:38:51 +0000

    deploy: bb1821f6d4b9a8e55e832d40222776323613e927

---
+++ index.search.js
@@ -0,0 +1,10 @@
+var relearn_search_index=[{content:`En introduktion för utveckling, installation och drift.
+Eller är det home som är till för den informationen?
+`,description:"",tags:null,title:"Introduktion",uri:"/00-intro/index.html"},{content:`Arkitekturbeskrivning.
+`,description:"",tags:null,title:"Arkitektur",uri:"/10-architecture/index.html"},{content:`Information om hur du enklast kommer igång.
+`,description:"",tags:null,title:"Komma igång",uri:"/20-getting-started/index.html"},{content:`Mer detaljerad information och konfigurationshjälp.
+`,description:"",tags:null,title:"Avancerad konfiguration",uri:"/30-advanced-config/index.html"},{content:"",description:"",tags:null,title:"Categories",uri:"/categories/index.html"},{content:`Om sakerna omkring dig kunde prata, vad skulle de säga?
+Introduktion Diwise diwise är en IoT-kommunikationsplattform för att ta emot sensordata och reglera fysiska styrsystem som innebär att verksamhetssystem frikopplas från både kommunikationsgränssnitt och olika sensortyper.
+Dokumentation Här på denna yta kommer vi publicera teknisk information samt dokumentation gällande plattformen Diwise.
+Kontakt Behöver du komma i kontakt med oss eller vill du veta mer om vad vi kan erbjuda? Läs mer på vår webbplats, diwise.se.
+`,description:"",tags:null,title:"Diwise",uri:"/index.html"},{content:"",description:"",tags:null,title:"Tags",uri:"/tags/index.html"}]
